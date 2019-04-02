39ad150b35bfb7fe3ed7fec5baa1c8142c778ed3
Zhenghao Jia <cherie-xia@hotmail.com>
2019-04-02 22:14:33 +0800

    new character in es6 object

--- sololearn/javascript course/index.js
+++ sololearn/javascript course/index.js
@@ -104,10 +104,224 @@ template literals are enclosed by the backtick `` instead of double or single qu
 The ${expression} is a placeholder, can include any expression, which will get inserted or evaluated into 
 the template literals
 */
-function templateLiterals() {
-    let a = 8;
-    let b = 4;
-    let msg = `The sum is ${a + b}`;
-    console.log(msg);
-}
-templateLiterals();//The sum is 12
+// function templateLiterals() {
+//     let a = 8;
+//     let b = 4;
+//     let msg = `The sum is ${a + b}`;
+//     console.log(msg);
+// }
+// templateLiterals();//The sum is 12
+
+//for in loops
+// let obj = {a : 1, b : 2, c : 3};
+// for (let v in obj) {
+//     console.log(v);//a,b,c
+//     console.log(obj[v]);//1,2,3
+//     console.log(obj.v);//undefined
+// }
+//for of loops
+// let list = ["x", "y", "z"];
+// for (let val of list) {
+//     console.log(val);//x,y,z
+// }
+// for (let ch of "Hello") {
+//     console.log(ch);//H,e,l,l,o
+// }
+// function add(x, y) {
+//     var sum = x + y;
+//     console.log(sum);
+// }
+// //ES6 introduces a new syntax for writing functions.
+// const add = (x, y) => {
+//     let sum = x + y;
+//     console.log(sum);
+// }
+// //This new syntax can be quite handy when you just need a simple function with one argument.
+// const greet = x => "Welcome" + x;
+// //no parameters
+// const x = () => alert("hi");
+
+// var arr = [1,3,5,7,9];
+// arr.forEach(function(el){
+//     console.log(el * 2);
+// });
+// const arr2 = [2,4,6,8,10];
+// arr2.forEach(v => {
+//     console.log(v * 2);
+// });
+// const printOdds = (arr) => {
+//     arr.forEach(el => {
+//         if (el % 2 == 0) {
+//             console.log(el);
+//         }
+//     })
+// }
+// const arr3 = [1,2,3,4,5,6,7,8];
+// printOdds(arr3);
+
+// //default parameters
+// const test = (a, b =3, c = 19) => {
+//     return a + b + c;
+// }
+// console.log(test(1));//23
+
+//In es6, method can be in shorthand, no function, no :
+// let tree = {
+//     height: 10,
+//     color: 'green',
+//     grow() {
+//         this.height += 2;
+//     }
+// }
+// tree.grow();
+// console.log(tree.height);
+
+/*
+object in es6, can be simplified in this way, but remember number only duplicate, not pointed to 
+the actual memory address
+*/
+// let height = 5;
+// let health = 100;
+// let athlete = {
+//     height,
+//     health
+// }
+// console.log(athlete.health);//100
+// health = 50;
+// console.log(athlete.health);//100
+
+//array or object is otherwise
+// let arr = [1,2,3];
+// let obj = {x:1,y:2};
+// let combine = {
+//     arr,
+//     obj
+// }
+// console.log(combine.arr[1]);//2
+// console.log(combine.obj.x);//1
+// arr.shift();
+// console.log(arr);//2,3
+// console.log(combine.arr[1]);//3 not 2 ^-^
+// obj.x = 0;
+// console.log(combine.obj.x)//0 the same reason of arr
+
+//computed property in es6
+//example 1:
+// let prop = 'name';
+// let id = '1234';
+// let mobile = '15821881560';
+// let user = {
+//     [prop]: 'Jay Jarsper',
+//     [`user_${id}`]: `${mobile}`
+// }
+// console.log(user);//{name: "Jay Jarsper", user_1234: "15821881560"}
+// mobile = '13917797109';
+// console.log(user);//{name: "Jay Jarsper", user_1234: "15821881560"}
+// let user2 = {
+//     [prop]: 'Jay Jarsper',
+//     [`user_${id}`]: mobile
+// }
+// console.log(user2);//{name: "Jay Jarsper", user_1234: "13917797109"}
+// mobile = '13801788256';
+// console.log(user2);//{name: "Jay Jarsper", user_1234: "13917797109"}
+//example 2:
+// var i = 0;
+// var a = {
+//     ['foo' + ++i]: i,
+//     ['foo' + ++i]: i,
+//     ['foo' + ++i]: i
+// }
+// console.log(a);//{foo1: 1, foo2: 2, foo3: 3}
+// i = 0;
+// var b = {
+//     ['foo' + i++]: i,
+//     ['foo' + i++]: i,
+//     ['foo' + i++]: i
+// }
+// console.log(b);//{foo0: 1, foo1: 2, foo2: 3}
+// //example 3:
+// var param = 'size';
+// var config = {
+//     [param]: 12,
+//     ['mobile' + param.charAt(0).toUpperCase() + param.slice(1)]: 4
+// }
+// console.log(config);//{size: 12, mobileSize: 4}
+
+/*
+    ES6 provides a new method for Object, which is assign()
+    This is useful for creating a new object by combining several existing sources
+    Also helpful as creating a duplicate of an existing object
+*/
+//exapmle 1:
+// let person = {
+//     name: 'Jack',
+//     age: 18,
+//     sex: 'male'
+// };
+// let student = {
+//     name: 'Bob',
+//     age: 20,
+//     xp: '2'
+// }
+// let newStudent = Object.assign({}, person, student);
+// console.log(newStudent);//{name: "Bob", age: 20, sex: "male", xp: "2"}
+//As we can see, the third parameter overrides the second parameter, thus sequence is important 
+
+//example 2:
+/*
+    Basic knowledge in C, C++ or Java, helps us understand the reference of an object.
+    So, just create another variable cannot duplicate a new object, because no new memory address is created.
+    To avoid changing original object with the change of the new variable, we use assign()
+*/
+// let person = {
+//     name: 'Jack',
+//     age: 18,
+//     sex: 'male'
+// };
+// let newPerson = Object.assign({}, person);
+// person.name = 'Simon';
+// console.log(person);//{name: "Simon", age: 18, sex: "male"}
+// console.log(newPerson);//{name: "Jack", age: 18, sex: "male"}
+
+//example 3:
+// let person = {
+//     name: 'Jack',
+//     age: 18,
+//     sex: 'male'
+// };
+// let newPerson = Object.assign({}, person, { name: 'Peter' });
+// console.log(newPerson);//{name: "Peter", age: 18, sex: "male"}
+
+// let a = 1;
+// let b = 2;
+// console.log(a, b);//1 2
+
+/*
+    A frozen object can no longer be changed; 
+    freezing an object prevents new properties from being added to it, 
+    existing properties from being removed, prevents changing the enumerability, 
+    configurability, or writability of existing properties, 
+    and prevents the values of existing properties from being changed.
+*/
+// let obj1 = {
+//     x: 1,
+//     y: 2
+// }
+// let obj2 = Object.freeze(obj1);
+// obj2.x = 3;
+// console.log(obj2);//
+
+/*
+    Object.seal() method seals an object, preventing new properties from being added to it and 
+    marking all existing properties as non-configurable. 
+    Values of present properties can still be changed as long as they are writable.
+*/
+let object1 = {
+    property1: 42
+};
+Object.seal(object1);
+object1.property1 = 33;
+console.log(object1.property1);// 33
+delete object1.property1; // cannot delete when sealed
+console.log(object1.property1);// expected output: 33
+
