2f8075838a3420b928a632c4795f74671436372b
Zhenghao Jia <cherie-xia@hotmail.com>
2019-05-13 22:14:02 +0800

    vue get started

---
+++ sololearn/vue course/startVue/L01 Mounting an instance of Vue.js
@@ -0,0 +1,8 @@
+/*
+    Mounting an instance of Vue
+    When your view becomes visible to the user
+    use the el option
+*/
+var app = new Vue ({
+    el: "#app"
+});
---
+++ sololearn/vue course/startVue/L02 Lifecycle of a View.js
@@ -0,0 +1,23 @@
+/**
+ * new VTTCue()
+ * creation     
+ * mounting
+ * updating
+ * destroy
+ */
+/**
+ * hooks
+ * beforeCreate;created
+ * beforeMount;mounted
+ * beforeUpdate;update
+ * beforeDestroy;destroyed
+*/
+
+//destroy only happens when you deliberately call the destroy method
+
+/*
+1. Lifecycle begins when you instantiate
+2. Instance is created via Vue constructor
+3. Istallation and setup
+4. What makes Vue.js valuable
+*/
