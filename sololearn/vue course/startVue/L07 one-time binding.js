585d3420ec1219a9cb24bb5edc2601a89866d8b6
Zhenghao Jia <cherie-xia@hotmail.com>
2019-05-14 17:30:10 +0800

    vue get started

---
+++ sololearn/vue course/startVue/L07 one-time binding.js
@@ -0,0 +1,10 @@
+var growler = new Vue({
+    el: "#growler",
+    data: {
+        appName: "Growler"
+    }
+});
+
+//v-once renders element once
+//children only get rendered once
+//helps optimize view performance
