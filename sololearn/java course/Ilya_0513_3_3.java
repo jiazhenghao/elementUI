2fb44f62cfe3686b9d531a0cc5380bf1180d1c92
Zhenghao Jia <cherie-xia@hotmail.com>
2019-05-14 21:15:55 +0800

    java quiz

---
+++ sololearn/java course/Ilya_0513_3_3.java
@@ -0,0 +1,5 @@
+class Ilya_0513_3_3 {
+    public static void main(String[] args) {
+        System.out.println('h' + 'i');//209
+    }
+}
---
+++ sololearn/java course/project0514/alpha5and5.java
@@ -0,0 +1,12 @@
+package project0514;
+
+class alpha5and5 {
+    alpha5and5(){}
+    void alpha5and5() {
+        System.out.println("Welcome Excutor");
+    }
+    public static void main(String[] args) {
+        alpha5and5 e = new alpha5and5();
+        e.alpha5and5();
+    }
+}
