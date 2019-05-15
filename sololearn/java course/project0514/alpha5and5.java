aa3718812a8945f748042f11e53b9ad93141a731
Zhenghao Jia <cherie-xia@hotmail.com>
2019-05-15 21:19:43 +0800

    vue get started

--- sololearn/java course/project0514/alpha5and5.java
+++ sololearn/java course/project0514/alpha5and5.java
@@ -2,11 +2,11 @@ package project0514;
 
 class alpha5and5 {
     alpha5and5(){}
-    void alpha5and5() {
-        System.out.println("Welcome Excutor");
-    }
+    // void alpha5and5() {
+    //     System.out.println("Welcome Excutor");
+    // }
     public static void main(String[] args) {
-        alpha5and5 e = new alpha5and5();
-        e.alpha5and5();
+        //alpha5and5 e = new alpha5and5();
+        //e.alpha5and5();
     }
 }
