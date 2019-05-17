e14796e8127bcb5dfb99978fd5b7e77abad6117a
Zhenghao Jia <cherie-xia@hotmail.com>
2019-05-17 23:55:51 +0800

    vue get started

---
+++ sololearn/vue course/startVue/L39 compare filter to method.js
@@ -0,0 +1,14 @@
+/**
+ * Filters should 
+ 1. only take in a value and return a new value
+ 2. not change the value of any properties in a view
+ */
+
+// Filters are easier to read
+
+/**
+ * Filters are a great way to handle basic text transformations
+ * Computed properties are for more complex transformations
+ * Watchers are for asynchronous operations
+ */
+
