4b1c3ed2361c75b4e51c01cf148730341db1e3e6
Zhenghao Jia <cherie-xia@hotmail.com>
2019-05-26 12:38:05 +0800

    vue ui

--- sololearn/Vue Path/Beginner/developing faster with the vue cli/vue-plugin-demo/src/main.js
+++ sololearn/Vue Path/Beginner/developing faster with the vue cli/vue-plugin-demo/src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import './registerServiceWorker'
 import { createProvider } from './vue-apollo'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
   apolloProvider: createProvider(),
+  router,
   render: h => h(App)
 }).$mount('#app')
