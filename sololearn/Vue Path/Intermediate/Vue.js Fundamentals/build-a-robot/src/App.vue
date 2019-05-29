1dfb0a5958901d515587312390af31cd35900235
Zhenghao Jia <cherie-xia@hotmail.com>
2019-05-29 22:16:47 +0800

    vue fundamentals chapter 5

--- sololearn/Vue Path/Intermediate/Vue.js Fundamentals/build-a-robot/src/App.vue
+++ sololearn/Vue Path/Intermediate/Vue.js Fundamentals/build-a-robot/src/App.vue
@@ -4,17 +4,26 @@
       <nav>
         <ul>
           <li class="nav-item">
-            <router-link class="nav-link" :to="{ name:'Home' }">
+            <!-- exact用来使得白色字体不是都存在 -->
+            <router-link class="nav-link" :to="{ name:'Home' }" exact>
               <img class="logo" src="./assets/build-a-bot-logo.png" alt>
               Build-a-bot
             </router-link>
           </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ name:'Build' }" exact>Build</router-link>
+          </li>
         </ul>
       </nav>
     </header>
-    <main>
-      <router-view/>
-    </main>
+    <div class="container">
+      <aside class="aside">
+        <router-view name="sidebar"/>
+      </aside>
+      <main>
+        <router-view/>
+      </main>
+    </div>
   </div>
 </template>
 
@@ -43,15 +52,14 @@ body {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
 }
 main {
-  margin: 0 auto;
   padding: 30px;
   background-color: white;
-  width: 1024px;
+  width: 964px;
   min-height: 300px;
 }
 header {
   background-color: #999;
-  width: 1084px;
+  width: 1184px;
   margin: 0 auto;
 }
 ul {
@@ -71,5 +79,22 @@ ul {
 .nav-link {
   text-decoration: none;
   color: inherit;
+}
+/*
+ 这是一个特别的css class
+*/
+.router-link-active {
+  color: white;
+}
+.container {
+  display: flex;
+  margin: 10px auto 0 auto;
+  justify-content: center;
+}
+.aside {
+  padding: 30px;
+  background-color: #aaa;
+  width: 100px;
+  min-height: 300px;
 }
 </style>
--- sololearn/Vue Path/Intermediate/Vue.js Fundamentals/build-a-robot/src/build/RobotBuilder.vue
+++ sololearn/Vue Path/Intermediate/Vue.js Fundamentals/build-a-robot/src/build/RobotBuilder.vue
@@ -83,10 +83,19 @@ import CollapsibleSection from "../shared/CollapsibleSection.vue";
 
 export default {
   name: "RobotBuilder",
+  beforeRouteLeave(to, from, next) {
+    if (this.addedToCart) {
+      next(true);
+    } else {
+      const response = confirm('You have not added your robot to your cart, are you sure you want to leave?');
+      next(response);
+    }
+  },
   components: { PartSelector, CollapsibleSection },
   data() {
     return {
       availableParts,
+      addedToCart: false,
       cart: [],
       selectedRobot: {
         head: {},
@@ -120,6 +129,7 @@ export default {
         robot.rightArm.cost +
         robot.base.cost;
       this.cart.push(Object.assign({}, robot, { cost }));
+      this.addedToCart = true;
     }
   }
 };
