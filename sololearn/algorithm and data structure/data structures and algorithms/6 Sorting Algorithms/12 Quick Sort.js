0f8173b6f8ea9f96b008875caab94191f633149c
Zhenghao Jia <cherie-xia@hotmail.com>
2019-06-04 06:55:30 +0800

    Divide and Conquer

--- sololearn/algorithm and data structure/data structures and algorithms/6 Sorting Algorithms/12 Quick Sort.js
+++ sololearn/algorithm and data structure/data structures and algorithms/6 Sorting Algorithms/12 Quick Sort.js
@@ -2,7 +2,7 @@ const randomArrayGenerator = require('./randomArrayGenerator');
 /*
     To test
 */
-var array = randomArrayGenerator(200000);
+var array = randomArrayGenerator(20);
 console.log(array);
 
 function quickSort(arr, left, right) {
@@ -13,6 +13,7 @@ function quickSort(arr, left, right) {
 
     if (left < right) {
         partitionIndex = partition(arr, left, right);
+        //console.log(partitionIndex);
         quickSort(arr, left, partitionIndex - 1);
         quickSort(arr, partitionIndex + 1, right);
     }
@@ -25,10 +26,19 @@ function partition(arr, left, right) {     // 分区操作
     for (var i = index; i <= right; i++) {
         if (arr[i] < arr[pivot]) {
             swap(arr, i, index);
+            //console.log(i + ' i');
+            //console.log(index + ' index');
             index++;
         }
+        
     }
+    // console.log(left);//0
+    // console.log(right);//9
+    
     swap(arr, pivot, index - 1);
+    console.log('index ' + index);
+    //console.log('pivot ' + pivot);
+    console.log(arr);
     return index - 1;
 }
 
@@ -36,13 +46,14 @@ function swap(arr, i, j) {
     var temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
+    //console.log(arr);
 }
 
 var begin = new Date();
 var arr = quickSort(array);
 var end = new Date();
-console.log(end - begin);
-console.log(arr);
+//console.log(end - begin);
+//console.log(arr);
 
 //length = 200000; time = 35
 //length = 100000; time = 29
