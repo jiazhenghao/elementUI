b56c0e858b6d135d64fbf8a1f72c045c249ae0b0
Zhenghao Jia <cherie-xia@hotmail.com>
2019-06-09 14:44:05 +0800

    34. Find First and Last Po of Ele in Sorted Array

---
+++ leetcode/34. Find First and Last Position of Element in Sorted Array/solution.js
@@ -0,0 +1,105 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+    if (nums.length == 0 || target < nums[0] || target > nums[nums.length - 1])
+        return [-1, -1];
+    if (nums.length == 1 && target == nums[0])
+        return [0, 0];
+    let start = 0;
+    let end = nums.length - 1;
+    let answer = [];
+
+    findout(nums, 0, nums.length - 1, target, answer);
+
+    if (answer[0] != undefined && answer[1] == undefined) {
+        return [answer[0], answer[0]];
+    }
+    if (answer[1] != undefined && answer[0] == undefined) {
+        return [answer[1], answer[1]];
+    }
+
+    return answer;
+};
+
+
+function findout(nums, start, end, target, answer) {
+    let median = Math.floor((start + end) / 2);
+    // console.log('start:'+start+' end:'+end);
+    // console.log('median: ' + median);
+
+    if (target == nums[start]) {
+        answer[0] != undefined ? answer[0] = Math.min(start, answer[0]) : answer[0] = start;
+        // if (answer[0] != undefined) {
+        //     answer[0] = Math.min(start, answer[0]);
+        // }
+        // else {
+        //     answer[0] = start;
+        // }
+    }
+    if (target < nums[start]) {
+        answer[0] = -1;
+    }
+    if (target == nums[end]) {
+        answer[1] != undefined ? answer[1] = Math.max(end, answer[1]) : answer[1] = end;
+        // if (answer[1] != undefined) {
+        //     answer[1] = Math.max(end, answer[1]);
+        // }
+        // else {
+        //     answer[1] = end;
+        // }
+    }
+    if (target > nums[end]) {
+        answer[1] = -1;
+    }
+    //console.log(answer);
+
+    if (end - start == 1) {
+        if (target != nums[start] && target != nums[end]) {
+            answer[0] = -1;
+            answer[1] = -1;
+            return;
+        }
+        return;
+    }
+
+    if (end - start == 0) {
+        return;
+    }
+
+    if (target > nums[median]) {
+        start = median + 1;
+        findout(nums, start, end, target, answer);
+    }
+
+    if (target < nums[median]) {
+        end = median - 1;
+        findout(nums, start, end, target, answer);
+    }
+
+    if (target == nums[median]) {
+        if (answer[0] != undefined) {
+            answer[0] = Math.min(median, answer[0]);
+        }
+        else {
+            answer[0] = median;
+        }
+        if (answer[1] != undefined) {
+            answer[1] = Math.max(median, answer[1]);
+        }
+        else {
+            answer[1] = median;
+        }
+
+        //answer[1] = ;
+        findout(nums, start, median, target, answer);
+        findout(nums, median, end, target, answer);
+    }
+}
+console.log(searchRange([5,7,7,8,8,10], 8));//[3,4]
+console.log(searchRange([5,7,7,8,8,10], 6));//[-1,-1]
+console.log(searchRange([1,3], 1));//[0, 0]
+console.log(searchRange([0,0,1,1,1,4,5,5], 2));//[-1,-1]
+console.log(searchRange([0,0,1,1,1,2,2,3,3,3,4,4,4,4,5,5,6,6,6,8,10,10], 4));//[10,13]
