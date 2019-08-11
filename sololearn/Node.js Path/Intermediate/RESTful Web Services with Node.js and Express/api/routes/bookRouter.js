542efe2237ed4a9c40700535855033892abf2411
Zhenghao Jia <cherie-xia@hotmail.com>
2019-08-11 12:47:33 +0800

    update

--- sololearn/Node.js Path/Intermediate/RESTful Web Services with Node.js and Express/api/routes/bookRouter.js
+++ sololearn/Node.js Path/Intermediate/RESTful Web Services with Node.js and Express/api/routes/bookRouter.js
@@ -1,36 +1,66 @@
 const express = require('express');
+const booksController = require('../controllers/booksController');
 function routes(Book) {
     const bookRouter = express.Router();
+    const controller = booksController(Book);
     bookRouter.route('/books')
-        .post((req, res) => {
-            const book = new Book(req.body);
-            book.save();
-            console.log(book);
-            return res.status(201).json(book);
-        })
-        .get((req, res) => {
-            //http://localhost:4000/api/books?genre=Fantasy
-            const query = {};
-            if (req.query.genre) {
-                query.genre = req.query.genre;
-            }
-            Book.find(query, (err, books) => {
-                if (err) {
-                    return res.send(err);
-                }
-                return res.json(books);
-            });
-        });
+        .post(controller.post)
+        .get(controller.get);
 
-    bookRouter.route('/books/:bookId').get((req, res) => {
-        //http://localhost:4000/api/books/5d4e502cc500186313d711d1
+    bookRouter.use('/books/:bookId', (req, res, next) => {
         Book.findById(req.params.bookId, (err, book) => {
             if (err) {
                 return res.send(err);
             }
-            return res.json(book);
+            if (book) {
+                req.book = book;
+                return next();
+            }
+            return res.sendStatus(404);
         });
     });
+
+    bookRouter.route('/books/:bookId') //http://localhost:4000/api/books/5d4e502cc500186313d711d1
+        .get((req, res) => res.json(req.book))
+        .put((req, res) => {
+            const { book } = req;
+            book.title = req.body.title;
+            book.author = req.body.author;
+            book.genre = req.body.genre;
+            book.read = req.body.read;
+            req.book.save((err) => {
+                if (err) {
+                    return res.send(err);
+                }
+                return res.json(book);
+            });
+        })
+        .patch((req, res) => {
+            const { book } = req;
+            if (req.book._id) {
+                delete req.body._id;
+            }
+            Object.entries(req.body).forEach((item) => {
+                const key = item[0];
+                const value = item[1];
+                book[key] = value;
+            });
+            req.book.save((err) => {
+                if (err) {
+                    return res.send(err);
+                }
+                return res.json(book);
+            });
+        })
+        .delete((req, res) => {
+            req.book.remove((err) => {
+                if (err) {
+                    return res.send(err);
+                }
+                return res.sendStatus(204);
+            })
+        });
+
     return bookRouter;
 }
 
