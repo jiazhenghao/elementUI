3d4d898d3da998fa9ac6f0c2d1672143133b72d6
Zhenghao Jia <cherie-xia@hotmail.com>
2019-08-17 18:28:36 +0800

    vue-fundamentals

--- sololearn/Vue Path/Intermediate/Vue.js Fundamentals/build-a-robot/src/build/RobotBuilder.vue
+++ sololearn/Vue Path/Intermediate/Vue.js Fundamentals/build-a-robot/src/build/RobotBuilder.vue
@@ -67,7 +67,7 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 export default {
   name: 'RobotBuilder',
   created() {
-    this.$store.dispatch('getParts');
+    this.$store.dispatch('robots/getParts');
   },
   beforeRouteLeave(to, from, next) {
     if (this.addedToCart) {
@@ -95,7 +95,7 @@ export default {
   mixins: [createdHookMixin],
   computed: {
     availableParts() {
-      return this.$store.state.parts;
+      return this.$store.state.robots.parts;
     },
     saleBorderClass() {
       return this.selectedRobot.head.onSale ? 'sale-border' : '';
@@ -116,7 +116,8 @@ export default {
         + robot.torso.cost
         + robot.rightArm.cost
         + robot.base.cost;
-      this.$store.commit('addRobotToCart', Object.assign({}, robot, { cost }));
+      this.$store.dispatch('robots/addRobotToCart', Object.assign({}, robot, { cost }))
+        .then(() => this.$router.push('/cart'));
       this.addedToCart = true;
     },
   },
