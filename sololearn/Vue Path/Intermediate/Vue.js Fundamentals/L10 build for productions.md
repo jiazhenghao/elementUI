e1797028eccbe3f0bf2047621176c87863ff71dd
Zhenghao Jia <cherie-xia@hotmail.com>
2019-08-18 15:46:19 +0800

    vuejs fundamentals summary

--- sololearn/Vue Path/Intermediate/Vue.js Fundamentals/L10 build for productions.md
+++ sololearn/Vue Path/Intermediate/Vue.js Fundamentals/L10 build for productions.md
@@ -3,3 +3,6 @@ npm run build -- --mode=production
 also you can
 npm run build -- --mode=development
 npm run build -- --mode=staging
+
+vue-cli 默默就有了webpack的配置，如果需要输出的化，需要如下命令
+vue inspect --mode=production > webpack.config.js
--- sololearn/Vue Path/Intermediate/Vue.js Fundamentals/build-a-robot/vue.config.js
+++ sololearn/Vue Path/Intermediate/Vue.js Fundamentals/build-a-robot/vue.config.js
@@ -1,4 +1,31 @@
 module.exports = {
+  configureWebpack: (config) => {
+    config.module.rules.push({
+      test: /\.coffee$/,
+      use: ['coffee-loader'],
+    });
+    const newRule = {
+      test: /\.(png|jpe?g|gif|webp)(\?.*)?$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            fallback: {
+              loader: 'file-loader',
+              options: {
+                name: 'img/[name].[hash:8].[ext]',
+              },
+            },
+          },
+        },
+      ],
+    };
+    const imagesRuleIndex = config.module.rules
+      .findIndex(x => x.test.source.includes('png|jpe?g|gif'));
+    config.module.rules.splice(imagesRuleIndex, 1, newRule);
+  },
+
   devServer: {
     proxy: {
       '/api': {
